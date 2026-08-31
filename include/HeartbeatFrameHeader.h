aed8385a2e3e5dec0c0a0df370384fe1e41cc91f
nobukoba <kobayash@rcnp.osaka-u.ac.jp>
2026-08-31 16:00:17 +0900

    Add heartbeat frame header

---
+++ include/HeartbeatFrameHeader.h
@@ -0,0 +1,17 @@
+#ifndef HBFHeader_H
+#define HBFHeader_H
+
+#include <cstdint>
+
+namespace HeartbeatFrame {
+constexpr uint64_t MAGIC = 0x0054414542545248;
+#pragma pack(4)
+struct Header {
+    uint64_t magic {MAGIC};
+    uint32_t length {0};
+    uint16_t hLength {16};
+    uint16_t type {0};
+};
+#pragma pack()
+} // namespace HeartbeatFrame
+#endif
